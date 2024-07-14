9276ca8d03d363c1e432f88506df6c2d1739cbce
WearsomeKarma <pahprios@gmail.com>
2024-07-14 11:15:25 -0500

    fixed tile rendering issues

--- core/source/world/tile.c
+++ core/source/world/tile.c
@@ -21,20 +21,24 @@ static Tile inline *get_p_tile_from__chunk_node_for__tile_render(
     p_north =
         (y__local == 0)
         ? get_p_tile_from__chunk_node_using__u8(
-                p_chunk_node->p_south__chunk_map_node,
-                x__local, CHUNK_WIDTH__IN_TILES - 1)
+                p_chunk_node->p_north__chunk_map_node,
+                x__local, 
+                CHUNK_WIDTH__IN_TILES - 1)
         : get_p_tile_from__chunk_node_using__u8(
                 p_chunk_node,
-                x__local, y__local - 1)
+                x__local, 
+                y__local - 1)
         ;
     p_south =
         (y__local == CHUNK_WIDTH__IN_TILES - 1)
         ? get_p_tile_from__chunk_node_using__u8(
-                p_chunk_node->p_north__chunk_map_node,
-                x__local, 0)
+                p_chunk_node->p_south__chunk_map_node,
+                x__local, 
+                0)
         : get_p_tile_from__chunk_node_using__u8(
                 p_chunk_node,
-                x__local, y__local + 1)
+                x__local, 
+                y__local + 1)
         ;
     p_west =
         (x__local == 0)
@@ -43,16 +47,19 @@ static Tile inline *get_p_tile_from__chunk_node_for__tile_render(
                 CHUNK_WIDTH__IN_TILES - 1, y__local)
         : get_p_tile_from__chunk_node_using__u8(
                 p_chunk_node,
-                x__local - 1, y__local)
+                x__local - 1, 
+                y__local)
         ;
     p_east =
         (x__local == CHUNK_WIDTH__IN_TILES - 1)
         ? get_p_tile_from__chunk_node_using__u8(
                 p_chunk_node->p_east__chunk_map_node,
-                0, y__local)
+                0, 
+                y__local)
         : get_p_tile_from__chunk_node_using__u8(
                 p_chunk_node,
-                x__local + 1, y__local)
+                x__local + 1, 
+                y__local)
         ;
 
     Tile_Wall_Adjacency_Code__u16 wall_adjacency = 0;
