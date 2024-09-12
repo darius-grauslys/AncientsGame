dfac88f80536620de6e6b0a58e9e51e6cbfc72a7
WearsomeKarma <pahprios@gmail.com>
2024-09-12 14:43:49 -0500

    window shows

--- sdl/source/sdl_main.c
+++ sdl/source/sdl_main.c
@@ -1,5 +1,27 @@
+#include <SDL2/SDL_events.h>
+#include <SDL2/SDL_video.h>
 #include <platform.h>
+#include <SDL2/SDL.h>
 
 int PLATFORM_main(Game *game) {
+    SDL_Init(
+            SDL_INIT_TIMER
+            | SDL_INIT_VIDEO
+            | SDL_INIT_AUDIO
+            | SDL_INIT_EVENTS);
+    SDL_Window *p_SDL_window = 
+        SDL_CreateWindow(
+            "AncientsGame", 0, 0, 800, 600, 
+            SDL_WINDOW_OPENGL
+            | SDL_WINDOW_SHOWN);
+    while(true) {
+        SDL_Event event;
+        if (SDL_PollEvent(&event)
+                && event.type == SDL_QUIT) {
+            break;
+        }
+    }
+    SDL_DestroyWindow(p_SDL_window);
+    SDL_Quit();
     return 0;
 }
